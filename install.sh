8803554e0ce638735058979600ae5d20822c22d5
eheea <eheea3333@gmail.com>
2024-10-09 00:53:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,3 +22,15 @@ mkfs.ext4 -F /dev/"$disk"2
 mount /dev/"$disk"2 /mnt
 mkdir -p /mnt/boot/efi
 mount /dev/"$disk"1 /mnt/boot/efi
+
+#installing the base system
+pacstrap -K /mnt base base-devel linux linux-firmware grub efibootmgr networkmanager nano neofetch
+
+#generating fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+#going into the newly installed system
+arch-chroot /mnt
+
+#system configs
+ln -sf /usr/share/zoneinfo/Asia/Baghdad /etc/localtime
