1cb3df583f725c1f161b9d22f735092de1bd102b
eheea <eheea3333@gmail.com>
2024-10-09 00:06:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 echo "please insert disk name (eg; sda,sdb)"
 read -r disk
-fdisk /dev/"$disk"
-echo "g"
-echo "n"
-echo ""
-echo ""
-echo "+1G"
-echo "n"
-echo ""
-echo ""
-echo ""
-echo "w"
+#creating a GPT partition table with 2 partitions for boot and root
+{ echo "g"
+  echo "n"
+  echo ""
+  echo ""
+  echo "+1G"
+  echo "n"
+  echo ""
+  echo ""
+  echo ""
+  echo "w" 
+} | fdisk /dev/"$disk"
+
+#formatting the disks
+mkfs.fat -F -F32 /dev/"$disk"1
+mkfs.ext4 -F /dev/"$disk"2
+
+#mounting the disks
+mkdir -p /mnt/boot/efi
+mount /dev/"$disk"1 /mnt/boot/efi
+mount /dev/"$disk"2 /mnt
