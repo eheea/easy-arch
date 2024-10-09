745a2e46964c48a2713de51cd32c971f4554f403
eheea <eheea3333@gmail.com>
2024-10-09 08:26:27 +0300

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+echo "please insert disk name (eg; sda,sdb)"
+read -r disk
+echo "        "
+
+echo "insert the user name"
+read -r username
+echo "        "
+
+echo "please insert your root password"
+read -r rootpasswd
+echo "        "
+
+echo "please insert your user password"
+read -r userpasswd
+
+#going into the newly installed system
+arch-chroot /mnt
+
+#clock config
+ln -sf /usr/share/zoneinfo/Asia/Baghdad /etc/localtime
+hwclock --systohc
+
+#locale config
+locale-gen
+echo "LANG=en_US.UTF-8" >> locale.conf
+
+#Network Configuration
+echo "arch" >> /etc/hostname
+systemctl enable NetworkManager
+
+#setting root password
+{ echo "$rootpasswd"
+echo "$rootpasswd"
+
+} | passwd
+
+#installing grub
+mkdir -p /boot/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+grub-install /dev/"$disk"
+
+#adding a user
+useradd -m -G wheel,input,users -s /bin/bash "$username"
+{ echo "$userpasswd"
+echo "$userpasswd"
+
+} | passwd "$username"
+
+#making the user able to use sudo
+SUDO="/etc/sudoers.d/my_custom_sudoers"
+if [ -f "$SUDO" ]; then
+    echo "File $SUDO already exists. Exiting."
+    exit 1
+fi
+
+echo "$username ALL=(ALL) NOPASSWD: ALL" > "$SUDO"
+chmod 440 "$SUDO"
+visudo -c
+echo "Custom sudoers file created successfully."
+sleep 1
+
+#enabling multilib
+sed -i 's/^#\[multilib\]/[multilib]/' /etc/pacman.conf
+sed -i 's/^#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist/' /etc/pacman.conf
+sudo pacman -Sy
+echo "Multilib repository enabled and package database updated."
+
+#installing the AUR helper
+if [ ! -f /usr/bin/yay ]; then
+sudo pacman -S --noconfirm  --needed git go base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
+else echo "yay is already installed"
+fi
+
+#installing the desktop and apps
+sudo pacman -Sy --noconfirm gnome gdm neofetch fastfetch gedit go samba sane cups flatpak kitty bluez bluez-utils timeshift btop vlc vulkan-radeon lib32-vulkan-radeon gnome-tweaks fuse wget
+yay -S --needed --noconfirm arch-gaming-meta thorium-browser-bin vesktop ttf-ms-fonts auto-cpufreq protonup-qt
+flatpak install -y flathub com.mattjakeman.ExtensionManager
+flatpak install -y flathub it.mijorus.gearlever
+flatpak install -y flathub io.github.peazip.PeaZip
+flatpak install -y flathub com.dec05eba.gpu_screen_recorder
+sudo systemctl enable gdm
+echo "operation is finished. rebooting.."
