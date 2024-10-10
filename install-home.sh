79a2798a152c6e33a153a5b76d4e7a1d386f3fca
eheea <eheea3333@gmail.com>
2024-10-10 08:36:28 +0300

    Create install-home.sh

---
+++ install-home.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+lsblk
+echo "please insert disk name (eg; sda,sdb)"
+read -r disk
+echo "        "
+
+
+echo "insert the user name"
+read -r username
+echo "        "
+
+echo "insert the host name"
+read -r host
+echo "     "
+
+echo "please insert your root password"
+read -r rootpasswd
+echo "        "
+
+echo "please insert your user password"
+read -r userpasswd
+echo "     "
+
+#making partitions for gpt
+umount -a
+{ echo "g"
+  echo "n"
+  echo ""
+  echo ""
+  echo "+1G"
+  echo "n"
+  echo ""
+  echo ""
+  echo "+10G"
+  echo "n"
+  echo " "
+  echo " "
+  echo " "
+  echo "w" 
+} | fdisk /dev/"$disk"
+
+#formatting the disks
+mkfs.ext4 -F /dev/"$disk"2
+mkfs.fat -F32 /dev/"$disk"1
+mkfs.ext4 -F /dev/"$disk"3
+
+#mounting the disks
+mount /dev/"$disk"2 /mnt
+mkdir -p /mnt/boot/efi
+mount /dev/"$disk"1 /mnt/boot/efi
+mkdir -p /mnt/home
+mount /dev/"$disk"3 /mnt/home
+
+#installing the base system
+pacstrap -K /mnt base base-devel linux linux-firmware grub efibootmgr networkmanager nano neofetch
+
+#generating fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+echo "fstab was generated successfully"
+
+#going into the newly installed system
+arch-chroot /mnt << EOF
+ln -sf /usr/share/zoneinfo/Asia/Baghdad /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8" >> /etc/locale.gen
+locale-gen
+echo "en_US.UTF-8" >> /etc/locale.conf
+echo "$host" >> /etc/hostname
+mkswap -U clear --size 4G --file /swapfile
+swapon /swapfile
+echo "/swapfile none swap default 0 0" >> /etc/fstab
+{
+echo "$rootpasswd"
+echo "$rootpasswd"
+} | passwd
+useradd -m -G wheel,input -s /bin/bash $username
+{
+echo "$userpasswd"
+echo "$userpasswd"
+} | passwd $username
+echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
+systemctl enable NetworkManager
+mkdir -p /boot/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+grub-install
+echo "     " >> /etc/pacman.conf
+echo "[multilib]" >> /etc/pacman.conf
+echo " Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+pacman -Sy
+su eheea
+mkdir /home/eheea/test
+cd /home/eheea/test
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+exit
+EOF
+umount -a
