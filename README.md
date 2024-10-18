9d6dfef23b70090f6bce9255ab3095d8e14161c7
eheea <eheea3333@gmail.com>
2024-10-18 18:57:07 +0300

    full release

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# arch-eheea
- - Warning:this is not finished please do not use
+# easy-arch
+ - this is basically a script that installs base arch. feel free to add desktops and window managers as you please
 
- - arch installation script that installs arch with gnome and apps and extensions that i personally use
+ - note: you need git installed using sudo pacman -S git
+ - usage is simple. just type 'git clone https://github.com/eheea/easy-arch && cd easy-arch && ./install.sh' and answer the questions
+ - small note if youre mounting a new /home disk. please partition it using 'cfdisk /dev/diskname' and format it using 'mkfs.ext4 /dev/diskname' then use the script
--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo -e "\e[32mdo you want a separate /home type install (basically separating the home folder on another partition or disk for backup purposes)\e[0m"
+echo "1-yes"
+echo "2-no"
+read -r hi
+case $hi in
+1) 
 lsblk
 echo "please insert disk name (eg; sda,sdb)"
 read -r disk
@@ -129,9 +135,110 @@ cd /home/eheea/test
 sudo rm -rf /home/$username/test/yay
 sudo pacman -Sy --noconfirm git go
 sudo pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
-yay -S auto-cpufreq --noconfirm && sudo auto-cpufreq --install && sudo auto-cpufreq force performance
+yay -S auto-cpufreq --noconfirm
 exit
 EOF
 echo "system was successfully installed.. rebooting"
 sleep 1
 umount -a
+reboot
+;;
+
+
+
+
+
+2)lsblk
+echo "please insert disk name (eg; sda,sdb)"
+read -r disk
+echo "        "
+
+
+echo "insert the user name"
+read -r username
+echo "        "
+
+echo "insert the host name"
+read -r host
+echo "     "
+
+echo "please insert your root password"
+read -r rootpasswd
+echo "        "
+
+echo "please insert your user password"
+read -r userpasswd
+echo "     "
+
+#making partitions for gpt
+umount -a
+{ echo "g"
+  echo "n"
+  echo ""
+  echo ""
+  echo "+1G"
+  echo "n"
+  echo ""
+  echo ""
+  echo ""
+  echo "w" 
+} | fdisk /dev/"$disk"
+
+#formatting the disks
+mkfs.ext4 -F /dev/"$disk"2
+mkfs.fat -F32 /dev/"$disk"1
+
+#mounting the disks
+mount /dev/"$disk"2 /mnt
+mkdir -p /mnt/boot/efi
+mount /dev/"$disk"1 /mnt/boot/efi
+
+#installing the base system
+pacstrap -K /mnt base base-devel linux linux-firmware grub efibootmgr networkmanager nano neofetch
+
+#generating fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+echo "fstab was generated successfully"
+
+#going into the newly installed system
+arch-chroot /mnt << EOF
+ln -sf /usr/share/zoneinfo/Asia/Baghdad /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "$host" >> /etc/hostname
+mkswap -U clear --size 4G --file /swapfile
+swapon /swapfile
+echo "/swapfile none swap default 0 0" >> /etc/fstab
+{
+echo "$rootpasswd"
+echo "$rootpasswd"
+} | passwd
+useradd -m -G wheel,input -s /bin/bash $username
+{
+echo "$userpasswd"
+echo "$userpasswd"
+} | passwd $username
+echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
+systemctl enable NetworkManager
+mkdir -p /boot/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+grub-install
+echo "     " >> /etc/pacman.conf
+echo "[multilib]" >> /etc/pacman.conf
+echo " Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+pacman -Sy
+su eheea
+mkdir /home/eheea/test
+cd /home/eheea/test
+sudo rm -rf /home/$username/test/yay
+sudo pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
+yay -S auto-cpufreq --noconfirm
+exit
+EOF
+umount -a
+echo "system was successfully installed.. rebooting"
+sleep 1
+reboot ;;
+esac
